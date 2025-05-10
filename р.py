2088158155114ab7456d930f9c2f8274e0c3987a
kein1245 <maximsim81@gmail.com>
2025-05-10 16:29:33 +0300

    Update р.py

--- р.py
+++ р.py
@@ -1,1 +1,17 @@
-print('сігма')
+print ('привіт а як тебе звуть?')
+
+NameUser = input('введіть імя:\n')
+print(NameUser)
+
+print('скільки тобі років?')
+vik_user = input('введіть вік:\n')
+print(vik_user)
+
+print ('твій колір?')
+
+colorUser = input('твій колір:\n')
+print(colorUser)
+
+print(NameUser)
+print(vik_user)
+print(colorUser)
